38f272bfd4f37abc71682f2693d1ba8d415b83b6
Julius Berger <julius.berger@gmail.com>
2020-03-26 23:46:50 +0100

    added parallax backdrop

---
+++ antibody/src/app/backdrop/backdrop.component.ts
@@ -0,0 +1,109 @@
+import { Component, HostListener, OnInit } from '@angular/core'
+
+
+interface Layer {
+  amount: number,
+  moveDepth: number,
+  displayDepth: number,
+  items: {
+    baseX: number,
+    baseY: number
+  }[]
+}
+
+@Component({
+  selector: 'app-backdrop',
+  templateUrl: './backdrop.component.html',
+  styleUrls: ['./backdrop.component.scss']
+})
+export class BackdropComponent implements OnInit {
+  e: MouseEvent | {x: number, y:number} = {x:0, y:0}
+
+  readonly imageBaseSize = 200
+
+  @HostListener('window:mousemove', ['$event'])
+  handleMouseMove(event: MouseEvent) {
+    this.e = event
+  }
+
+
+  layers: Layer[] = [
+    {
+      amount: 100,
+      moveDepth: 11,
+      displayDepth: 15,
+      items: []
+    },
+    {
+      amount: 50,
+      moveDepth: 8,
+      displayDepth: 12,
+      items: []
+    },
+    {
+      amount: 20,
+      moveDepth: 6,
+      displayDepth: 9,
+      items: []
+    },
+    {
+      amount: 15,
+      moveDepth: 5,
+      displayDepth: 7,
+      items: []
+    },
+    {
+      amount: 8,
+      moveDepth: 4,
+      displayDepth: 5,
+      items: []
+    },
+    {
+      amount: 5,
+      moveDepth: 3,
+      displayDepth: 3,
+      items: []
+    },
+    {
+      amount: 1,
+      moveDepth: 2,
+      displayDepth: 2,
+      items: []
+    },
+    {
+      amount: 1,
+      moveDepth: 2.4,
+      displayDepth: 2.4,
+      items: []
+    },
+    {
+      amount: 1,
+      moveDepth: 2.6,
+      displayDepth: 2.6,
+      items: []
+    }
+  ]
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.populateLayers()
+  }
+
+
+  populateLayers() {
+    function r() {
+        return Math.floor(Math.random() * 100)
+    }
+
+    this.layers.forEach(layer => {
+      for (let i = 0; i < layer.amount; i++) {
+        layer.items.push({
+          baseX: r(),
+          baseY: r()
+        })
+      }
+    })
+  }
+}
