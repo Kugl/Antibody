d178fd3007d2c8e4e74a044efcc9ed05227ecdd8
Kugl <michael.eisenbart@gmx.de>
2020-03-27 10:29:43 +0100

    Added class to describe Antibodys

--- antibody/src/app/services/central.service.ts
+++ antibody/src/app/services/central.service.ts
@@ -1,37 +1,35 @@
-import { Injectable } from '@angular/core';
-import { makeDefaultGame } from '../core/gameFactory'
-import { Game } from '../core/game'
+import { Injectable } from "@angular/core";
+import { makeDefaultGame } from "../core/gameFactory";
+import { Game } from "../core/game";
+import { Card } from "../core/card";
 
 @Injectable({ providedIn: "root" })
 export class CentralService {
-
-  game: Game
+  game: Game;
 
   constructor() {
-    this.game = makeDefaultGame()
+    this.game = makeDefaultGame();
     // Game loop below
     // TODO: proper timing logic -- simple interval will lead to variable game speeds
-    let that = this
+    let that = this;
     setInterval(function() {
       that.game.tick();
-    },
-    60)
+    }, 60);
   }
 
   getGame() {
-    return this.game
+    return this.game;
   }
 
   drawCard() {
-    this.game.drawCard()
+    this.game.drawCard();
   }
 
-  playCard(card) {
-    this.game.playCard(card)
+  playCard(card: Card) {
+    this.game.playCard(card);
   }
 
-  discardCard(card) {
-    this.game.discardCard(card)
+  discardCard(card: Card) {
+    this.game.discardCard(card);
   }
-
 }
