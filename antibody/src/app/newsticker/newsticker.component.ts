74f9071db8e94d6507941bd0caba8f7e93d29d67
Moritz Gronbach <meismo@gmail.com>
2020-03-27 15:13:10 +0100

    fix more merge issues

--- antibody/src/app/newsticker/newsticker.component.ts
+++ antibody/src/app/newsticker/newsticker.component.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { interval, timer } from 'rxjs'
 import { NewsMessage } from '../core/newsTicker'
 import { CentralService } from '../services/central.service';
-=======
-import { Component, OnInit } from "@angular/core";
-import { interval, timer } from "rxjs";
-import { CentralService } from "../services/central.service";
-import { NewsMessage } from "../core/message";
->>>>>>> ee2bf1e77ca9e13605c762da3d6f4ff3991c1e1f
 
 @Component({
   selector: "app-newsticker",
@@ -21,19 +14,13 @@ export class NewstickerComponent implements OnInit {
   readonly MINIMUM_OPCAITY = 0.3;
   readonly OPACITY_DECREASE_FACTOR = 10.0;
 
-<<<<<<< HEAD
   constructor(centralService: CentralService){
     this.news = centralService.getGame().newsTicker.news;
    }
 
   ngOnInit(): void {
-=======
-  constructor(centralService: CentralService) {
-    this.news = centralService.getGame().news;
->>>>>>> ee2bf1e77ca9e13605c762da3d6f4ff3991c1e1f
   }
 
-  ngOnInit(): void {}
 
   max = Math.max;
 }
