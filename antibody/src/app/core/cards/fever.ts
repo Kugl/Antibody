34bd340b1d76b3b699992f16ad5368ad8dc1e3c2
Kugl <michael.eisenbart@gmx.de>
2020-03-27 10:36:54 +0100

    Added proposal for card layout

--- antibody/src/app/core/cards/fever.ts
+++ antibody/src/app/core/cards/fever.ts
@@ -3,6 +3,24 @@ import { Card } from "../card";
 
 export function makeFeverCard() {
   let effects = [new IncreaseTemperature()];
-  const card = new Card("Fever", "assets/pictures/corona.jpg", "Increase body temperature.", effects);
+  const card = new Card(
+    "Fever",
+    "assets/pictures/corona.jpg",
+    "Increase body temperature.",
+    effects
+  );
   return card;
 }
+
+//Proposal: Different Syntax for cards based on my work on the disease definition
+
+export class DummyFeverCard implements Card {
+  title = "Fever";
+  pictureURL = "assets/pictures/corona.jpg";
+  text = "This is teh Cards text";
+  effects = [new IncreaseTemperature()];
+}
+
+//Should work, as we do not change the cards dynamically. Although we can if need be.
+// can be called with new DummyFeverCard().
+//I like this a bit better for readability. Please comment
