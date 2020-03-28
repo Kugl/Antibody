8f8277171ab764a00043a38baf46ee6d11aaa11a
Moritz Gronbach <meismo@gmail.com>
2020-03-28 13:03:34 +0100

    fix spelling of leukocytosis

--- antibody/src/app/core/cards/leukocytosis.ts
+++ antibody/src/app/core/cards/leukocytosis.ts
@@ -5,6 +5,6 @@ import { IncreaseLeukosProduction } from '../effects/increaseLeukosProduction';
 export class LeukocytosisCard extends Card {
   title = "Leukocytosis";
   pictureURL = "assets/pictures/leukos.jpg";
-  text = "Increase Leucocyte Production x5.";
+  text = "Increase Leukocyte Production x5.";
   effects = [new IncreaseLeukosProduction()];
 }
