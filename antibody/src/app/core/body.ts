70606e6a373e8197dd18a7ab429dd3a573afbc5d
Kugl <michael.eisenbart@gmx.de>
2020-03-28 10:31:59 +0100

    Tought the defenders how to fight

--- antibody/src/app/core/body.ts
+++ antibody/src/app/core/body.ts
@@ -1,14 +1,14 @@
 import { WhiteBloodcell, TCell } from "./immune-system/antibodys";
 import { Disease, makeDiseaseArray } from "./diseases/diseases";
-import { TicksPerDay } from "./constants"
-import { DefensePool } from "./defense"
+import { TicksPerDay } from "./constants";
+import { DefensePool } from "./defense";
 import { NewsTicker } from "./newsTicker";
-import { factorDay2Tick } from './util';
+import { factorDay2Tick } from "./util";
 
 export class Body {
-  fever: boolean = false
-  temperature: number = 37
-  mucusProduction: number = 0
+  fever: boolean = false;
+  temperature: number = 37;
+  mucusProduction: number = 0;
 
   diseases: Disease[];
   defensePool: DefensePool;
@@ -22,23 +22,24 @@ export class Body {
   }
 
   tick() {
-    this.handleInfections()
-    this.defensePool.grow()
-    this.mobilize()
+    this.handleInfections();
+    this.defensePool.grow();
+    this.mobilize();
+    this.fightDiseases();
   }
 
   mobilize() {
-    const rate = 1.0 - factorDay2Tick(1.0 - this.mobilizationRate) // the subtrahend is what we want to keep per tick
-    for(let disease of this.diseases) {
-      if(disease.Count < 1) {
-        continue
+    const rate = 1.0 - factorDay2Tick(1.0 - this.mobilizationRate); // the subtrahend is what we want to keep per tick
+    for (let disease of this.diseases) {
+      if (disease.Count < 1) {
+        continue;
       }
-      for(let defender of disease.defenders) {
-        for(let poolDefender of this.defensePool.defenders) {
+      for (let defender of disease.defenders) {
+        for (let poolDefender of this.defensePool.defenders) {
           if (defender.name === poolDefender.name) {
-            const transfer = rate * poolDefender.count
-            defender.count += transfer
-            poolDefender.count -= transfer
+            const transfer = rate * poolDefender.count;
+            defender.count += transfer;
+            poolDefender.count -= transfer;
           }
         }
       }
@@ -48,21 +49,33 @@ export class Body {
   handleInfections() {
     for (let disease of this.diseases) {
       if (disease.Count > 0) {
-        disease.spread()
+        disease.spread();
       } else {
-        this.maybeGetInfected(disease)
+        this.maybeGetInfected(disease);
       }
     }
   }
 
-
   maybeGetInfected(disease) {
-      // +0.01 is for testing only, to have more frequent infections
+    // +0.01 is for testing only, to have more frequent infections
     if (Math.random() < disease.ChanceOfInfection / TicksPerDay + 0.01) {
-      disease.infect()
-      this.newsTicker.publishNews("You have been infected with " + disease.Name);
+      disease.infect();
+      this.newsTicker.publishNews(
+        "You have been infected with " + disease.Name
+      );
     }
   }
 
-
+  //Goes through all diseases. If defenders are present each defender fights once.
+  fightDiseases() {
+    for (let disease of this.diseases) {
+      if (disease.Count > 0) {
+        if (disease.defenders.length > 0) {
+          for (let defender of disease.defenders) {
+            defender.fight(disease);
+          }
+        }
+      }
+    }
+  }
 }
--- antibody/src/app/core/defense.ts
+++ antibody/src/app/core/defense.ts
@@ -1,48 +1,63 @@
-import { factorDay2Tick, linearRateDay2Tick } from './util';
-
+import { factorDay2Tick, linearRateDay2Tick } from "./util";
+import { Disease } from "./diseases/diseases";
 
 export class Defender {
   name: string;
   count: number;
   production: number;
   decay: number;
-
+  //Fraction of Tcells that kil la virus on a day.
+  combatPower: number;
 
   grow() {
-    const tickSurvival = factorDay2Tick(1.0 - this.decay)
-    const tickProduction = linearRateDay2Tick(this.production)
-    this.count = this.count * tickSurvival + tickProduction
+    const tickSurvival = factorDay2Tick(1.0 - this.decay);
+    const tickProduction = linearRateDay2Tick(this.production);
+    this.count = this.count * tickSurvival + tickProduction;
+  }
+  //TODO: Adapt to accopunt for different fighting styles
+  fight(disease: Disease): void {
+    let strength = this.count * this.combatPower;
+    //Specific Tcells only fight the virus they are build for
+    disease.Count = disease.Count - strength;
+    if (disease.Count < 0) {
+      disease.Count = 0;
+    }
   }
 }
 
 export class TCells extends Defender {
-  name = "T-Cells"
+  name = "T-Cells";
   count = 0;
   production = 50;
   decay = 0.1;
+  //Fraction of Tcells that kil la virus on a day.
+  combatPower: number = 0.1;
 }
 
 export class Leukos extends Defender {
-  name = "Leukocytes"
+  name = "Leukocytes";
   count = 0;
   production = 200;
   decay = 0.05;
+  //Fraction of Tcells that kil la virus on a day.
+  combatPower: number = 0.1;
 }
 
 export class Macrophages extends Defender {
-  name = "Macrophages"
+  name = "Macrophages";
   count = 0;
   production = 1;
   decay = 0.01;
+  //Fraction of Tcells that kil la virus on a day.
+  combatPower: number = 0.1;
 }
 
 export class DefensePool {
-  defenders: Defender[] = [new TCells(), new Leukos(), new Macrophages()]
+  defenders: Defender[] = [new TCells(), new Leukos(), new Macrophages()];
 
   grow() {
-    for(let defender of this.defenders) {
-      defender.grow()
+    for (let defender of this.defenders) {
+      defender.grow();
     }
   }
-
 }
