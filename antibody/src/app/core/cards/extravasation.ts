f81a03bc2f79afd45ad722b1a4aa50aa1b055aa6
Moritz Gronbach <meismo@gmail.com>
2020-03-28 13:35:51 +0100

    fix name of extravasation card

--- antibody/src/app/core/cards/extravasation.ts
+++ antibody/src/app/core/cards/extravasation.ts
@@ -3,7 +3,7 @@ import { Extravasation } from '../effects/extravasation';
 
 
 export class ExtravasationCard extends Card {
-  title = "Leukocytosis";
+  title = "Extravasation";
   pictureURL = "assets/pictures/extravasation.png";
   text = "Increase Leukocyte Mobilization x5.";
   effects = [new Extravasation()];
