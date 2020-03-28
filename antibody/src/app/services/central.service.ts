47df68618583478ded02d190b016c8f21ac2e85a
Moritz Gronbach <meismo@gmail.com>
2020-03-28 22:12:07 +0100

    implement better main loop logic to ensure tick length=60ms in most situations

--- antibody/src/app/services/central.service.ts
+++ antibody/src/app/services/central.service.ts
@@ -13,16 +13,21 @@ export interface CardPlayedEvent {
 export class CentralService {
   game: Game;
 
+  tickLength = 60;
+  lastTime: number;
+
   CardSubject = new Subject<CardPlayedEvent>();
+
+  startTime: number;
 
   constructor() {
     this.game = makeDefaultGame();
+    this.startTime = new Date().getTime();
+    this.lastTime = this.startTime;
     // Game loop below
     // TODO: proper timing logic -- simple interval will lead to variable game speeds
-    let that = this;
-    setInterval(function() {
-      that.game.tick();
-    }, 60);
+    const that = this;
+    this.mainLoop(that)
   }
 
   getGame() {
@@ -43,4 +48,25 @@ export class CentralService {
     this.game.discardCard(card);
     this.CardSubject.next({ Action: "Discard", card });
   }
+
+
+  mainLoop(that) {
+    let dateObject = new Date();
+    let currentTime = dateObject.getTime();
+    console.log("mainloop", (currentTime - this.startTime) / that.game.clock.tickCount)
+    let tickLength = that.tickLength;
+    console.log(currentTime-that.lastTime)
+    let timeDiff = currentTime - that.lastTime;
+    if (tickLength < timeDiff) {
+      that.game.tick();
+      that.lastTime += tickLength;
+      timeDiff -= tickLength;
+    }
+    if (tickLength < timeDiff) { // catch up
+      setTimeout(() => that.mainLoop(that), 5)
+    } else {
+      setTimeout(() => that.mainLoop(that), Math.min(1000, tickLength / 3))
+    }
+  }
+
 }
