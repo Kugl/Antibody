3cb0766e4f44e7f2ea6514beeba620ded4e46c26
Kugl <michael.eisenbart@gmx.de>
2020-03-28 13:35:41 +0100

    Status display added

--- antibody/src/app/main-view/status/status.component.ts
+++ antibody/src/app/main-view/status/status.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { CentralService } from "src/app/services/central.service";
 import { Game } from "src/app/core/game";
 import { Effect } from "src/app/core/effects/effect";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-status",
@@ -11,10 +12,16 @@ import { Effect } from "src/app/core/effects/effect";
 export class StatusComponent implements OnInit {
   currentGame: Game;
   effects: Effect[];
+  private sub: Subscription;
 
   constructor(private centServ: CentralService) {
     this.currentGame = this.centServ.getGame();
-    this.effects = this.currentGame.effects;
+
+    this.sub = this.centServ.CardSubject.subscribe(event => {
+      if (event.Action == "Play") {
+        this.effects = this.currentGame.effects;
+      }
+    });
   }
 
   ngOnInit(): void {}
--- antibody/src/app/services/central.service.ts
+++ antibody/src/app/services/central.service.ts
@@ -2,10 +2,18 @@ import { Injectable } from "@angular/core";
 import { makeDefaultGame } from "../core/gameFactory";
 import { Game } from "../core/game";
 import { Card } from "../core/card";
+import { Subject } from "rxjs";
+
+export interface CardPlayedEvent {
+  Action: string;
+  card?: Card;
+}
 
 @Injectable({ providedIn: "root" })
 export class CentralService {
   game: Game;
+
+  CardSubject = new Subject<CardPlayedEvent>();
 
   constructor() {
     this.game = makeDefaultGame();
@@ -23,13 +31,16 @@ export class CentralService {
 
   drawCard() {
     this.game.drawCard();
+    this.CardSubject.next({ Action: "Draw" });
   }
 
   playCard(card: Card) {
     this.game.playCard(card);
+    this.CardSubject.next({ Action: "Play", card });
   }
 
   discardCard(card: Card) {
     this.game.discardCard(card);
+    this.CardSubject.next({ Action: "Discard", card });
   }
 }
