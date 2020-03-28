fc15e9964af6b03f48765e6032abeb8eb530daf2
Kugl <michael.eisenbart@gmx.de>
2020-03-28 11:55:36 +0100

    rebalanced mucus to last longer

--- antibody/src/app/core/effects/mucusProduction.ts
+++ antibody/src/app/core/effects/mucusProduction.ts
@@ -1,16 +1,16 @@
-import { Effect } from './effect'
-import { Game } from '../game'
+import { Effect } from "./effect";
+import { Game } from "../game";
 
 export class MucusProduction implements Effect {
-  displayText = "Increase Mucus Production"
-  action = ""
-  duration = 20
+  displayText = "Increase Mucus Production";
+  action = "";
+  duration = 100;
 
   activate(game: Game) {
-    game.body.mucusProduction += 1
+    game.body.mucusProduction += 1;
   }
 
   deactivate(game: Game) {
-    game.body.mucusProduction -= 1
+    game.body.mucusProduction -= 1;
   }
 }
