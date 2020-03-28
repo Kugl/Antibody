3c24a3c3addd7a1daad15c34b00b4b01b7cf9702
Kugl <michael.eisenbart@gmx.de>
2020-03-28 15:47:29 +0100

    one file missed

--- antibody/src/app/core/effects/extravasation.ts
+++ antibody/src/app/core/effects/extravasation.ts
@@ -1,19 +1,18 @@
-import { Effect } from './effect';
-import { Game } from '../game';
-import { TicksPerDay } from '../constants';
-
-
+import { Effect } from "./effect";
+import { Game } from "../game";
+import { TicksPerDay } from "../constants";
 
 export class Extravasation implements Effect {
-  displayText = "Increase Leukocyte Mobilization Rate x5"
-  action = ""
-  duration = TicksPerDay * 1
+  displayText = "Increase Leukocyte Mobilization Rate x5";
+  action = "";
+  duration = TicksPerDay * 1;
+  readonly maxDuration = TicksPerDay * 1;
 
   activate(game: Game) {
-    game.body.defensePool.leukos.mobilizationRate *= 5
+    game.body.defensePool.leukos.mobilizationRate *= 5;
   }
 
   deactivate(game: Game) {
-    game.body.defensePool.leukos.mobilizationRate /= 5
+    game.body.defensePool.leukos.mobilizationRate /= 5;
   }
 }
