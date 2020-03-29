165b5e2125c39383b5fde47062c95953fdffcb3f
Moritz Gronbach <meismo@gmail.com>
2020-03-29 12:20:18 +0200

    delete old effect versions

--- antibody/src/app/core/cards/extravasation.ts
+++ antibody/src/app/core/cards/extravasation.ts
@@ -1,11 +1,10 @@
-import { Card } from '../card';
-import { Extravasation } from '../effects/extravasation';
-import { EffectFactory } from '../effects/effect';
-
+import { Card } from "../card";
+import { EffectFactory } from "../effects/effect";
+import { MobilizeEffect } from "../effects/mobilize-effect";
 
 export class ExtravasationCard extends Card {
   title = "Extravasation";
   pictureURL = "assets/pictures/extravasation.png";
   text = "Increase Leukocyte Mobilization x5.";
-  effectFactories = [new EffectFactory(Extravasation)];
+  effectFactories = [new EffectFactory(MobilizeEffect, ["tCells", "T-Cell"])];
 }
--- antibody/src/app/core/cards/leukocytosis.ts
+++ antibody/src/app/core/cards/leukocytosis.ts
@@ -1,10 +1,12 @@
-import { Card } from '../card';
-import { IncreaseLeukosProduction } from '../effects/increaseLeukosProduction';
-import { EffectFactory } from '../effects/effect'
+import { Card } from "../card";
+import { EffectFactory } from "../effects/effect";
+import { IncreaseDefenderProduction } from "../effects/increaseDefenderProduction";
 
 export class LeukocytosisCard extends Card {
   title = "Leukocytosis";
   pictureURL = "assets/pictures/leukos.jpg";
   text = "Increase Leukocyte Production x5.";
-  effectFactories = [new EffectFactory(IncreaseLeukosProduction)];
+  effectFactories = [
+    new EffectFactory(IncreaseDefenderProduction, ["leukos", "Leukocyte"])
+  ];
 }
