855ef08d001cc793f8a6a9bb3642b0303330db97
Kugl <michael.eisenbart@gmx.de>
2020-03-29 10:10:38 +0200

    mobilize effect added

---
+++ antibody/src/app/core/effects/mobilize-effect.ts
@@ -0,0 +1,23 @@
+import { Effect } from "./effect";
+import { Game } from "../game";
+import { TicksPerDay } from "../constants";
+
+export class MobilizeEffect implements Effect {
+  displayText = "Increase $ { targetDefenderName } Mobilization Rate x5";
+  action = "";
+  duration = TicksPerDay * 1;
+  readonly maxDuration = TicksPerDay * 1;
+
+  constructor(
+    private targetDefender: string,
+    private targetDefenderName: string
+  ) {}
+
+  activate(game: Game) {
+    game.body.defensePool[this.targetDefender].mobilizationRate *= 5;
+  }
+
+  deactivate(game: Game) {
+    game.body.defensePool[this.targetDefender].mobilizationRate /= 5;
+  }
+}
