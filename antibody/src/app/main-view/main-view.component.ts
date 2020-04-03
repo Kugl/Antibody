169ed4c86f2901dc88e2bd6aca845ebc159fe28b
Kugl <michael.eisenbart@gmx.de>
2020-04-03 14:29:22 +0200

    Sorting reenabled

--- antibody/src/app/main-view/main-view.component.ts
+++ antibody/src/app/main-view/main-view.component.ts
@@ -75,7 +75,7 @@ export class MainViewComponent implements OnInit, AfterContentChecked {
   ngAfterContentChecked() {
     this.sumDeadliness();
     //TODO: Refactor t oa place where istsorts less often
-    //this.orderDeadly();
+    this.orderDeadly();
     this.health = 100 - this.totalDeadliness;
   }
 
